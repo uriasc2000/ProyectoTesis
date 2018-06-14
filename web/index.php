1638cd5a26cce77f9a9183d996a276887ed5a74e
marvin urias <sistema.localizar.gps@gmail.com>
2018-06-14 12:06:39 -0600

    Formato de codigo

--- web/index.php
+++ web/index.php
@@ -1,29 +1,14 @@
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
     <head>
-     
-    	<title>Localizador linkit one</title>
-     
-    		<script type="text/javascript" src="/jquery/jquery.min.js"></script>
-
-            <script type="text/javascript" src="/jquery/funciones.js"></script>
-     
-     
-    <script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBTOZpHPXJ1Z6m_WYQ6YRtLoHznv5vOvqE'></script>
-     
-     
-    		
-     
+        <title>Localizador linkit one</title>
+        <script type="text/javascript" src="/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="/jquery/funciones.js"></script>
+        <script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBTOZpHPXJ1Z6m_WYQ6YRtLoHznv5vOvqE'></script>
     </head>
-     
     <body>
-     
-     
-    <div><h3 style="color:#3498db";>LOCALIZADOR</h3></div>
-     
-    <div id="map" style="width:50%;height:500px; border-radius:5px;border:1px solid  #3498db ;"></div>
-     
-     <div id="log">initial content</div>
-     
+        <div><h3 style="color:#3498db">LOCALIZADOR</h3></div>
+        <div id="map" style="width:50%;height:500px; border-radius:5px;border:1px solid  #3498db ;"></div>
+        <div id="log">initial content</div>
     </body>
-    </html>
+</html>
