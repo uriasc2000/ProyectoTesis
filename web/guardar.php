0c9d438f909b10ed8d461c220b8fe94cf0f413dc
marvin urias <sistema.localizar.gps@gmail.com>
2018-06-15 09:53:34 -0600

    Agrega conexion a la base de datos

--- web/guardar.php
+++ web/guardar.php
@@ -13,6 +13,22 @@ $coordenadas = array('latitud' => $latitud, 'longitud' => $longitud, 'velocidad'
 $json_string = json_encode($coordenadas);
 $file = 'coordenadas.json';
 file_put_contents($file, $json_string);
-
 //GUARDAR EN BASE DE DATOS LO OBTENIDO
+//CREAR CONEXION
+//CREDENCIALES BASE DE DATOS
+$HOST = "ec2-50-16-241-91.compute-1.amazonaws.com";
+$DB = "d9760bupnbe5kt";
+$USUARIO = "eubqinvrqabscc";
+$PASSWORD = "e04c3d34b6d0be5c97027021c77cdfd81c62c89a123f40adba4f26c3a049f392";
+$PORT = "5432";
+
+$cadenaConexion = "host=$HOST port=$PORT dbname=$DB user=$USUARIO password=$PASSWORD";
+
+$conexion = pg_connect($cadenaConexion) or die("ERROR DE CONEXION: ".pg_last_error());
+
+$query = "insert into localizador.entradas values($latitud,$longitud,$velocidad)";
+
+$resultado = pg_query($conexion,$query)or die("error en consulta SQL");
+
+pg_clientencoding($conexion);
 ?>
