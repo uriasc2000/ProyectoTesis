b232630307f883b8cc73a7287147a5174002c928
marvin urias <sistema.localizar.gps@gmail.com>
2018-06-20 17:13:40 -0600

    Intento 4

--- web/guardar.php
+++ web/guardar.php
@@ -14,16 +14,16 @@ $json_string = json_encode($coordenadas);
 $file = 'coordenadas.json';
 file_put_contents($file, $json_string);
 //GUARDAR EN BASE DE DATOS LO OBTENIDO
-$link = mysql_connect('http://soluciones502.com', 'root', 'Admin123')or die('No se pudo conectar: ' . mysql_error());
-mysql_select_db('localizadordb') or die('No se pudo seleccionar la base de datos');
-
-// Realizar una consulta MySQL
-$query = 'insert into localizador.entradas values($latitud,$longitud,$velocidad)';
-$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
-
-// Liberar resultados
-mysql_free_result($result);
-
-// Cerrar la conexión
-mysql_close($link);
+//$link = mysql_connect('http://soluciones502.com', 'root', 'Admin123')or die('No se pudo conectar: ' . mysql_error());
+//mysql_select_db('localizadordb') or die('No se pudo seleccionar la base de datos');
+//
+//// Realizar una consulta MySQL
+//$query = 'insert into localizador.entradas values($latitud,$longitud,$velocidad)';
+//$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+//
+//// Liberar resultados
+//mysql_free_result($result);
+//
+//// Cerrar la conexión
+//mysql_close($link);
 ?>
