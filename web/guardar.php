abbb7d17916f0a36570a90e710e747ae6ba5668c
Marvin Urias <localizadorlinkitone@gmail.com>
2018-06-20 23:07:29 -0600

    mostrar errores

--- web/guardar.php
+++ web/guardar.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 $latitud = $_GET['lati']; //obtenemos la latitud por URL
 
 $longitud = $_GET['longi']; //obtenemos la longitud por URL
