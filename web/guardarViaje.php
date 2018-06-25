0444f4018c0126abc114fa20328126aaa2eb50d2
Marvin Urias <localizadorlinkitone@gmail.com>
2018-06-24 22:30:08 -0600

    Test new form

--- web/guardarViaje.php
+++ web/guardarViaje.php
@@ -19,15 +19,22 @@ if($inicioViaje==1){//Inicio del viaje
     $conexion = mysqli_connect($servidor,$usuario,$contrasena)or die("No se pudo conectar:");
     $db = mysqli_select_db($conexion,$basededatos) or die("No se pudo seleccionar la base de datos");
     // Realizar una consulta MySQL
-    $query_c = "CALL ADD_VIAJE('$placa',@salida)";
+    $query_c = "CALL ADD_NEW_VIAJE('$placa',@salida)";
     $query_r = "select @salida";
     $result = mysqli_query($conexion,$query_c) or die("Consulta fallida 1:" . mysqli_error($conexion) );
     $result_id = mysqli_query($conexion,$query_r) or die("Consulta fallida 2:" . mysqli_error($conexion) );
     $respuesta = mysqli_fetch_array($result_id);
     $id_viaje = $respuesta["@salida"];
     echo $id_viaje;
+    mysqli_close($conexion);
 }elseif($inicioViaje==0){//Continual el viaje
-    echo "continuara" . $id_viaje;
+    $conexion = mysqli_connect($servidor,$usuario,$contrasena)or die("No se pudo conectar:");
+    $db = mysqli_select_db($conexion,$basededatos) or die("No se pudo seleccionar la base de datos");
+    // Realizar una consulta MySQL
+    $query = "SELECT viaje_actual FROM ACTUALES WHERE placa = $placa";
+    $result = mysqli_query($conexion,$query) or die("Consulta fallida 1:" . mysqli_error($conexion) );
+    $respuesta = mysqli_fetch_array($result);
+    echo $respuesta;
 }
 
 ?>
