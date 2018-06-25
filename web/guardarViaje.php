7775fb46d4760c2e79ed60a0e201068979b54401
Marvin Urias <localizadorlinkitone@gmail.com>
2018-06-24 21:53:15 -0600

    Test new mode

--- web/guardarViaje.php
+++ web/guardarViaje.php
@@ -3,26 +3,31 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+$inicioViaje = $_GET['inicio']; //Averiguar si inicia el viaje
 $placa = $_GET['placabus']; //obtenemos la placa por URL
+
+//INICIA EL VIAJE
+
 //GUARDAR EN BASE DE DATOS LO OBTENIDO
 $usuario = "root";
 $contrasena = "Admin123+";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.
 $servidor = "35.237.166.125";
 $basededatos = "localizadordb";
-
-$conexion = mysqli_connect($servidor,$usuario,$contrasena)or die("No se pudo conectar:");
-$db = mysqli_select_db($conexion,$basededatos) or die("No se pudo seleccionar la base de datos");
-
-// Realizar una consulta MySQL
 $id_viaje = -1;
-$query_c = "CALL ADD_VIAJE('$placa',@salida)";
-$query_r = "select @salida";
-$result = mysqli_query($conexion,$query_c) or die("Consulta fallida 1:" . mysqli_error($conexion) );
-$result_id = mysqli_query($conexion,$query_r) or die("Consulta fallida 2:" . mysqli_error($conexion) );
-$respuesta = mysqli_fetch_array($result_id);
-$json_response = json_encode($respuesta);
-//echo("<!DOCTYPE html><html><body>" . $json_response . "</body></html>");
-echo($json_response);
-// Cerrar la conexión
-mysqli_close($conexion);
+
+if($inicioViaje==1){//Inicio del viaje
+    $conexion = mysqli_connect($servidor,$usuario,$contrasena)or die("No se pudo conectar:");
+    $db = mysqli_select_db($conexion,$basededatos) or die("No se pudo seleccionar la base de datos");
+    // Realizar una consulta MySQL
+    $query_c = "CALL ADD_VIAJE('$placa',@salida)";
+    $query_r = "select @salida";
+    $result = mysqli_query($conexion,$query_c) or die("Consulta fallida 1:" . mysqli_error($conexion) );
+    $result_id = mysqli_query($conexion,$query_r) or die("Consulta fallida 2:" . mysqli_error($conexion) );
+    $respuesta = mysqli_fetch_array($result_id);
+    $id_viaje = $respuesta["@salida"];
+    echo $id_viaje;
+}elseif($inicioViaje==0){//Continual el viaje
+    echo "continuara" . $id_viaje;
+}
+
 ?>
