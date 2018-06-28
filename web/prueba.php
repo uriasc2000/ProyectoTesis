751c6696503d61ca4f1949f14cf6fcdc23712373
marvin urias <sistema.localizar.gps@gmail.com>
2018-06-28 15:04:44 -0600

    Vamo a probar

--- web/prueba.php
+++ web/prueba.php
@@ -1,7 +1,5 @@
 <?php
-
 require("db_info.php");
-
 $placa = $_GET['placa']; //Averiguar si inicia el viaje
 
 error_reporting(E_ALL);
@@ -22,26 +20,7 @@ if (!$db_selected) {
 //Crear query para obtener datos
 $query = "select * from VIAJE where placa = '$placa' order by fecha desc";
 $result = mysqli_query($connection,$query)or die("Consulta fallida 4:" . mysqli_error($connection));
-
-header("Content-type: text/xml");
-
-// Start XML file, echo parent node
-echo "<?xml version='1.0' ?>";
-echo '<viajes>';
-$ind=0;
-// Iterate through the rows, printing XML nodes for each
-while ($row = @mysqli_fetch_assoc($result)){
-  // Add to XML document node
-  echo '<viaje ';
-  echo 'id="' . $row['ID'] . '" ';
-  echo 'fecha="' . $row['FECHA'] . '" ';
-  echo 'calificacion="' . $row['CALIFICACION'] . '" ';
-  echo 'placa="' . $row['PLACA'] . '" ';
-  echo '/>';
-  $ind = $ind + 1;
-}
-
-// End XML file
-echo '</viajes>';
+$respuesta = mysqli_fetch_array($result);
+echo json_encode($respuesta);
 mysqli_close($connection);
 ?>
