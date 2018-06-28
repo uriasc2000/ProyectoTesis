38514cca5dc0d338ed1eac6c0b412f8c1d632ef1
marvin urias <sistema.localizar.gps@gmail.com>
2018-06-28 15:16:07 -0600

    Test

---
+++ web/pruebatabla.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html >
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <title>Viaje</title>
+    <script type="text/javascript" src="jquery/jquery.min.js"></script>
+    <script type="text/javascript">
+      
+      $(function(){
+        $("#consultar").click(function(){
+          $("#receptor").text("consultando");
+          $.get("prueba.php",{placa:"C-123XYZ"},respuesta,'json')
+      });
+      });
+
+      function respuesta(arg) {
+        $("#receptor").html(arg);
+      }
+
+    </script>
+  </head>
+  <body>
+    <input type="button" id="consultar" value="consultar">
+    <div id="receptor"></div>
+  </body>
+</html>
