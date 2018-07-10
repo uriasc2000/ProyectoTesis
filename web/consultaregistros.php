4f25219fa31916f437f4c94902e420d0310558a9
Marvin Urias <localizadorlinkitone@gmail.com>
2018-07-10 00:22:50 -0600

    vamo a probar

--- web/consultaregistros.php
+++ web/consultaregistros.php
@@ -61,7 +61,7 @@
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" value="CONSULTAR">Submit</button>
 </form>
 
 
