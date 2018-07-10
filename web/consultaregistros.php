b72d125cad7cf8dd397be5125cbcbba0d0ba0d6b
Marvin Urias <localizadorlinkitone@gmail.com>
2018-07-10 00:44:54 -0600

    test

--- web/consultaregistros.php
+++ web/consultaregistros.php
@@ -50,9 +50,7 @@
       <label for="entrada" class="sr-only">Entrada</label>
       <input type="text" class="form-control" id="entrada">
     </div>
-  </form>
-  <form>
-  <input type="button" id="Consultar" value="CONSULTAR" class="btn btn-primary">
+    <input type="button" id="Consultar" value="CONSULTAR" class="btn btn-primary">
   </form>
   </div>
   <div id="receptor"></div>
