7d96988f4e2604f2004822c39805653d41630ba5
Marvin Urias <localizadorlinkitone@gmail.com>
2018-07-10 01:50:19 -0600

    test

--- web/index.php
+++ web/index.php
@@ -5,11 +5,16 @@
         <script type="text/javascript" src="/jquery/jquery.min.js"></script>
         <script type="text/javascript" src="/jquery/funciones.js"></script>
         <script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBTOZpHPXJ1Z6m_WYQ6YRtLoHznv5vOvqE'></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     </head>
     <body>
-        <div><h3 style="color:#3498db">RASTREO EN TIEMPO REAL</h3></div>
-        <div id="map" style="width:50%;height:500px; border-radius:5px;border:1px solid  #3498db ;"></div>
-        <div id="log">initial content</div>
-        <div id="logD"></div>
+        <div class="card">
+        <h5 class="card-header">SEGUIMIENTO EN TIEMPO REAL</h5>
+        <div class="card-body">
+            <center>
+                <div id="map" style="width:75%;height:500px; border-radius:5px;border:1px solid  #3498db ;"></div>    
+            </center>
+        </div>
+        </div>
     </body>
 </html>
