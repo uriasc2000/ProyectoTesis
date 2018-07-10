d80980c3c52950edbb0d36d96ba269c4879a72cd
Marvin Urias <localizadorlinkitone@gmail.com>
2018-07-10 01:24:53 -0600

    test

--- web/mimapa.php
+++ web/mimapa.php
@@ -4,6 +4,21 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>Mapa de recorrido</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+      * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  
   </head>
   <body>
     <div class="card">
@@ -12,7 +27,7 @@
         
       </div>
     </div>
-    <div id="map" style="width:50%;height:500px; border-radius:5px;border:1px solid  #3498db ;"></div>
+    <div id="map" ></div>
         <div id="ingreso">
           <form> 
             <?php
